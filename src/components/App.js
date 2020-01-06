bb01ba21d911c3fd37542de38bd77fb47a934686
Sophie MARCHAND <sophie.marchand@dazzl.tv>
2020-01-06 10:55:37 +0100

    first trials commits

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
-import React, { Component} from "react";
-import "../assets/jss/App.css";
+import React, { Component} from "react"
+import "../assets/jss/App.css"
+import logo from "../assets/img/logo_final.png"
+// const logo = require('../assets/img/logo_final.png');
 
 class App extends Component{
-  render(){
+
+  
+  render () {
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <img src={logo} alt="Logo" />
+        <h1> Site en construction </h1>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+// function App () {
+//   return (
+//     <div className="App">
+//       <h1> Hello, World! </h1>
+//     </div>
+//   )
+// }
+
+export default App
