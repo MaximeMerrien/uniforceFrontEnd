f85ff6a43fee74260825697e595d0dc96237f16c
Sophie MARCHAND <sophie.marchand@dazzl.tv>
2020-01-19 14:14:35 +0100

    feat(gamers): wip GamerCard component

--- src/views/GamerCard.js
+++ src/views/GamerCard.js
@@ -5,30 +5,12 @@ class GamerCard extends Component{
   render () {
     return(
       <div className="GamerCard">
-        <div className="card-header">
-          <div className="card-photo">Photo</div>
-          <div className="card-infos">
-            <h2 className="card-infos-title">{this.props.gamer.pseudo}</h2>
-            <div className="card-infos-content">
-              <div>Prénom: {this.props.gamer.firstname}</div>
-              <div>Nom: {this.props.gamer.lastname}</div>
-              <div>Age: {this.props.gamer.age}</div>
-            </div>
-          </div>
+        <div className="gamer-shield">
+          <div className="gamer-pseudo">{this.props.gamer.pseudo}</div>
+          <div className="gamer-photo"></div>
+          <div className="gamer-motto">{this.props.gamer.motto}</div>          
         </div>
-        <div className="card-header">
-          <div className="card-left-content">
-            <div>Other: {this.props.gamer.firstname}</div>
-            <div>Other: {this.props.gamer.lastname}</div>
-            <div>Other: {this.props.gamer.age}</div>
-          </div>
-          <div className="card-main-content">
-            <div className="card-description">
-              {this.props.gamer.description}
-            </div>
-
-          </div>
-        </div>       
+        <div className="gamer-description">{this.props.gamer.description}</div>       
       </div>
     )
   }
