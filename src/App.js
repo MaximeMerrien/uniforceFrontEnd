fae8c1c019810b0086322d3c291648edbf2ac159
segoleneimie <segolene.vincent@imie.fr>
2020-01-20 20:03:57 +0100

    ajout des éléments de la page d'accueil ajout des icones RS

--- src/App.js
+++ src/App.js
@@ -4,17 +4,33 @@ import {Animated} from "react-animated-css"
 
 class App extends Component{
 
-  
-  render () {
-    return(
-      <div className="App">
-        <Animated animationIn="flipInX" animationOut="fadeOut" isVisible={true}>
-          <img src={require('./assets/img/logo_final.png')} className="logo" alt="Logo" />
-        </Animated>        
-        <h1 className="waiting-text"> Site en construction</h1>
-      </div>
-    )
-  }
+
+    render () {
+        return(
+            <div className="App">
+                <div className="Header">
+                    <div className="LogoContain">
+                        <Animated animationIn="flipInX" animationOut="fadeOut" isVisible={true}>
+                            <img src={require('./assets/img/logo_final.png')} className="logo" alt="Logo" />
+                        </Animated>
+                        <div className="Icons">
+                            <a href="https://twitter.com/UniForceTV"><img src={require('./assets/img/icon/twitter.svg')} alt="logo twitter"/></a>
+                            <a href="https://instagram.com/tvuniforce"><img src={require('./assets/img/icon/instagram.svg')} alt="logo instagram"/></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="Menu">
+                    <a > Nos Joueurs </a>
+                </div>
+                <div>
+                    <h1 className="waiting-text"> Uniforce Tv </h1>
+                    <iframe src="https://player.twitch.tv/?channel=uniforce_tv" frameBorder="0" allowFullScreen="true"
+                            scrolling="no" height="378" width="620"/>
+                </div>
+
+            </div>
+        )
+    }
 }
 
 export default App
