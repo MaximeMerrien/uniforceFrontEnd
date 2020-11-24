36d55bc90582fcacd71d34edff4e0b2aa34c3f90
intouchsophie <intouchsophie>
2020-11-24 13:42:21 +0100

    add entire twitch player & chat

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,32 +1,48 @@
 import React, { Component} from 'react'
-// import "../assets/jss/Home.css"
+import "../assets/jss/Home.css"
 
 
 class Home extends Component{  
+  constructor () {
+    super()
+    console.log('DOM', document.getElementsByClassName('Twitch-player'))
+  }
+
   render () {
     return(
       <div className="Home">
         {/* <h1 className="Home-title">UNI-FORCE tv</h1> */}
         <div className="Twitch">
-          <iframe
-            src="https://player.twitch.tv/?channel=uniforce_tv&parent=localhost&muted=true"
-            height="720"
-            width="1280"
-            frameborder="0"
-            scrolling="no"
-            allowfullscreen="true"
-          />
+          <div className="Player-embed">
+            <div id="twitch-embed"></div>
+          </div>
         </div>
-        <div>
-          <iframe frameborder="0"
-          scrolling="no"
-          id="chat_embed"
-          src="https://www.twitch.tv/embed/uniforce_tv/chat?parent=localhost"
-          height="500"
-          width="350">
-          </iframe>
-        </div>
-        <div class="Push"></div>
+        {/* <div className="Twitch">
+          <div className="Twitch-channel-player">
+            <div className="Twitch-player">
+              <iframe
+                src="https://player.twitch.tv/?channel=uniforce_tv&parent=localhost&muted=true"
+                height="720"
+                width="1280"
+                frameborder="0"
+                scrolling="no"
+                allowfullscreen="true"
+              />                
+            </div>
+          </div>
+          <div className="Twitch-channel-chat">
+            <div className="Twitch-chat">
+              <iframe
+                frameborder="0"
+                scrolling="no"
+                src="https://www.twitch.tv/embed/uniforce_tv/chat?parent=localhost&theme=dark"
+                height="500"
+                width="350"
+              />  
+            </div>
+          </div>
+        </div> */}
+        {/* <div class="Push"></div> */}
       </div>
     )
   }
